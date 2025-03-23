6f6cf6052b431772126abbb704b3e2c7b3467478
Yatharth Sathya <yascole2010@gmail.com>
2025-03-23 16:22:27 -0700

    frosh Social Sciences course selection

---
+++ lgSS.py
@@ -0,0 +1,15 @@
+
+
+froshSS = input("Do you want a harder, or easier History path? Type advanced, medium or easy.")
+
+if froshSS == "advanced":
+    print("""An advance course load in history must mean you really like it! Or, you're just really good at it. AP World History, AP US History, and then AP US Government & Politics/
+AP MacroEconomics or AP Psychology, your choice based on how you like this courseload so far. This is ideal for political science/economics majors, as you need these courses to lay the
+foundation for your college.""")
+elif froshSS == "medium":
+    print("""A medium course load is great! AP World History in sophomore year, US History: American Experience in junior, and then Economics. A good choice for any CS or natural science
+majors.""")
+elif froshSS == "easy":
+    print("""Simple! We can implement a schedule that includes World History, US History, and then Sociology Psychology in your Senior year. This sets up an easy history plan, perfect
+if you don't demonstrate much interest in the field, or if you're not quite as good at it.""")
+
